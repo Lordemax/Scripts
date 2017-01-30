88fb45c53555abf1e906d0c9bb19edd5a05ba24c
Lordemax <lordemax2012@gmail.com>
2017-01-30 02:17:52 +0100

    Update UserExpired.sh

--- UserExpired.sh
+++ UserExpired.sh
@@ -6,33 +6,53 @@ echo "" > /root/alluser.txt
 cat /etc/shadow | cut -d: -f1,8 | sed /:$/d > /tmp/expirelist.txt
 totalaccounts=`cat /tmp/expirelist.txt | wc -l`
 for((i=1; i<=$totalaccounts; i++ ))
-       do
-       tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
-       username=`echo $tuserval | cut -f1 -d:`
-       userexp=`echo $tuserval | cut -f2 -d:`
-       userexpireinseconds=$(( $userexp * 86400 ))
-       tglexp=`date -d @$userexpireinseconds`             
-       tgl=`echo $tglexp |awk -F" " '{print $3}'`
-       while [ ${#tgl} -lt 2 ]
-       do
-           tgl="0"$tgl
-       done
-       while [ ${#username} -lt 15 ]
-       do
-           username=$username" " 
-       done
-       bulantahun=`echo $tglexp |awk -F" " '{print $2,$6}'`
-       echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/alluser.txt
-       todaystime=`date +%s`
-       if [ $userexpireinseconds -ge $todaystime ] ;
-           then
-           timeto7days=$(( $todaystime + 604800 ))
-                if [ $userexpireinseconds -le $timeto7days ];
-                then                     
-                     echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/infouser.txt
-                fi
-       else
-       echo " User : $username Expire tanggal : $tgl $bulantahun" >> /root/expireduser.txt
-       passwd -l $username
-       fi
+do
+tuserval=`head -n $i /tmp/expirelist.txt | tail -n 1`
+username=`echo $tuserval | cut -f1 -d:`
+userexp=`echo $tuserval | cut -f2 -d:`
+userexpireinseconds=$(( $userexp * 86400 ))
+tglexp=`date -d @$userexpireinseconds` 
+tgl=`echo $tglexp |awk -F" " '{print $3}'`
+while [ ${#tgl} -lt 2 ]
+do
+tgl="0"$tgl
+done
+while [ ${#username} -lt 15 ]
+#! / Bin / bash
+echo ""> /root/infouser.txt
+echo ""> /root/expireduser.txt
+echo ""> /root/alluser.txt
+
+cat / etc / shadow | cut -d: -f1,8 | sed /: $ / d> /tmp/expirelist.txt
+totalaccounts = `cat /tmp/expirelist.txt | wc -l`
+for ((i = 1; i <= $ totalaccounts; i ++))
+       do
+       tuserval = `head -n $ i /tmp/expirelist.txt | tail -n 1`
+       username = `echo $ tuserval | F1 cut -d: `
+       userexp = `echo $ tuserval | -f2 cut -d: `
+       userexpireinseconds = $ (($ userexp * 86400))
+       tglexp = `date -d @ $ userexpireinseconds`
+       date = `echo $ tglexp | awk -F" " '{print $ 3}'`
+       while [$ {date} -lt 2]
+       do
+           date = "0" $ Date
+       done
+       while [$ {username} -lt 15]
+       do
+           username = $ username ' "
+       done
+       yearMonth = `echo $ tglexp | awk -F" " '{print $ 2, $ 6}'`
+       echo "User: $ username Expire date: $ Date $ yearMonth" /root/alluser.txt >>
+       todaystime = `date +% s`
+       if [$ userexpireinseconds -ge $ todaystime];
+           then
+           timeto7days = $ (($ todaystime + 604800))
+                if [$ userexpireinseconds -le $ timeto7days];
+                then
+                     echo "User: $ username Expire date: $ Date $ yearMonth" /root/infouser.txt >>
+                fi
+       else
+       echo "User: $ username Expire date: $ Date $ yearMonth" /root/expireduser.txt >>
+       $ passwd -l username
+       fi
 done
