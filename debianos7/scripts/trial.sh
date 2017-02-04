6e94d2af215f7ccb9409cc7def27600bdfaad657
Lordemax <lordemax2012@gmail.com>
2017-02-04 02:35:44 +0100

    Update trial.sh

--- debianos7/scripts/trial.sh
+++ debianos7/scripts/trial.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Script auto create trial user SSH
-#yg akan expired setelah 1 hari
+#Account will be expired after 1 day
 
 
 
 Login=TNL`</dev/urandom tr -dc X-Z0-9 | head -c4`
-hari="1"
+day = "1"
 Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 
 useradd -e `date -d "$hari days" +"%Y-%m-%d"` $Login
@@ -14,6 +14,6 @@ echo -e ""
 echo -e "==== SUCCESS ===="
 echo -e "Porta    : 443 (dropbear)"
 echo -e "Username : $Login"
-echo -e "Senha: $Pass\n"
+Echo -e "Password: $ Pass \ n"
 echo -e "==========================="
 echo -e ""
